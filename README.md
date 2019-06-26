d9c7122031f01a027e3997ccfa188249bfe66f2c
ttnguyen <tuannguyen.tran@orientsoftware.com>
2019-06-26 13:26:13 +0700

    first

---
+++ README.md
@@ -0,0 +1,38 @@
+# generator-commonjs-template [![NPM version][npm-image]][npm-url] [![Build Status][travis-image]][travis-url] [![Dependency Status][daviddm-image]][daviddm-url] [![Coverage percentage][coveralls-image]][coveralls-url]
+> generator common js config
+
+## Installation
+
+First, install [Yeoman](http://yeoman.io) and generator-commonjs-template using [npm](https://www.npmjs.com/) (we assume you have pre-installed [node.js](https://nodejs.org/)).
+
+```bash
+npm install -g yo
+npm install -g generator-commonjs-template
+```
+
+Then generate your new project:
+
+```bash
+yo commonjs-template
+```
+
+## Getting To Know Yeoman
+
+ * Yeoman has a heart of gold.
+ * Yeoman is a person with feelings and opinions, but is very easy to work with.
+ * Yeoman can be too opinionated at times but is easily convinced not to be.
+ * Feel free to [learn more about Yeoman](http://yeoman.io/).
+
+## License
+
+MIT © [ttnguyen]()
+
+
+[npm-image]: https://badge.fury.io/js/generator-commonjs-template.svg
+[npm-url]: https://npmjs.org/package/generator-commonjs-template
+[travis-image]: https://travis-ci.org//generator-commonjs-template.svg?branch=master
+[travis-url]: https://travis-ci.org//generator-commonjs-template
+[daviddm-image]: https://david-dm.org//generator-commonjs-template.svg?theme=shields.io
+[daviddm-url]: https://david-dm.org//generator-commonjs-template
+[coveralls-image]: https://coveralls.io/repos//generator-commonjs-template/badge.svg
+[coveralls-url]: https://coveralls.io/r//generator-commonjs-template
---
+++ generators/app/index.js
@@ -0,0 +1,163 @@
+"use strict";
+import Generator from "yeoman-generator";
+import { red } from "chalk";
+import yosay from "yosay";
+
+export default class extends Generator {
+  prompting() {
+    this.appname = this.appname.replace(/ /g, ".");
+    // Have Yeoman greet the user.
+    this.log(
+      yosay(
+        `Welcome to the cat\'s meow ${red(
+          "generator-commonjs-template"
+        )} generator!`
+      )
+    );
+
+    const prompts = [
+      {
+        type: "confirm",
+        name: "someAnswer",
+        message: "Would you like to enable this option?",
+        default: true
+      }
+    ];
+
+    return this.prompt(prompts).then(props => {
+      // To access props later use this.props.someAnswer;
+      this.props = props;
+    });
+  }
+
+  writing() {
+    if (this.appname !== this.props.appname) {
+      this.destinationRoot(`./${this.props.appname}`);
+    }
+
+    this._writingPackage();
+    this._writingReadme();
+    this._writingIndexHtml();
+    this._writingTsConfig();
+    this._writingGitIgnore();
+    this._writingLogo();
+    this._writingIndexBootstrapApp();
+    this._writingFavicon();
+    this._writingManifest();
+    this._writingApp();
+    this._writingEslint();
+
+    this.composeWith(require.resolve("generator-git-init"));
+  }
+
+  install() {
+    this.installDependencies({ bower: false });
+  }
+
+  _writingPackage() {
+    this.fs.copyTpl(
+      this.templatePath("_package.json"),
+      this.destinationPath("package.json"),
+      {
+        name: this.props.appname,
+        description: this.props.description
+      }
+    );
+  }
+
+  _writingReadme() {
+    this.fs.copyTpl(
+      this.templatePath("README.md"),
+      this.destinationPath("README.md"),
+      {
+        name: this.props.appname,
+        description: this.props.description
+      }
+    );
+  }
+
+  _writingIndexHtml() {
+    this.fs.copyTpl(
+      this.templatePath("public/index.html"),
+      this.destinationPath("public/index.html"),
+      {
+        name: this.props.appname
+      }
+    );
+  }
+
+  _writingTsConfig() {
+    this.fs.copy(
+      this.templatePath("tsconfig.json"),
+      this.destinationPath("tsconfig.json")
+    );
+  }
+
+  _writingGitIgnore() {
+    this.fs.copy(
+      this.templatePath("gitignore"),
+      this.destinationPath(".gitignore")
+    );
+  }
+
+  _writingLogo() {
+    this.fs.copy(
+      this.templatePath("src/logo.png"),
+      this.destinationPath("src/logo.png")
+    );
+  }
+
+  _writingApp() {
+    this.fs.copyTpl(
+      this.templatePath("src/App.tsx"),
+      this.destinationPath("src/App.tsx"),
+      {
+        name: this.props.appname
+      }
+    );
+
+    this.fs.copyTpl(
+      this.templatePath("src/App.test.tsx"),
+      this.destinationPath("src/App.test.tsx"),
+      {
+        name: this.props.appname
+      }
+    );
+  }
+
+  _writingIndexBootstrapApp() {
+    this.fs.copy(
+      this.templatePath("src/index.tsx"),
+      this.destinationPath("src/index.tsx")
+    );
+  }
+
+  _writingFavicon() {
+    this.fs.copy(
+      this.templatePath("public/favicon.ico"),
+      this.destinationPath("public/favicon.ico")
+    );
+  }
+
+  _writingManifest() {
+    this.fs.copyTpl(
+      this.templatePath("public/manifest.json"),
+      this.destinationPath("public/manifest.json"),
+      {
+        name: this.props.appname
+      }
+    );
+  }
+
+  _writingEslint() {
+    this.fs.copy(
+      this.templatePath("eslintrc"),
+      this.destinationPath(".eslintrc")
+    );
+
+    this.fs.copy(
+      this.templatePath("eslintignore"),
+      this.destinationPath(".eslintignore")
+    );
+  }
+}
